87b56a70e42179890013b5e091a962b4a3e171ff
Rohit Paul <113459757+RohitPaul0007@users.noreply.github.com>
2023-09-19 10:51:59 +0530

    Add files via upload

---
+++ Typing Project/assets/js/dark.js
@@ -0,0 +1,25 @@
+function change (iconID){
+    if(document.getElementById(iconID).className=="bx bx-moon dark-btn"){
+      document.getElementById(iconID).className = "bx bx-sun dark-btn";
+    }else{
+      document.getElementById(iconID).className = "bx bx-moon dark-btn";
+    }
+  }
+
+function toggleLightDarkMode() {
+    let body = document.body;
+    let element = document.querySelector("#element");
+    if (body.classList.contains("light-mode")) {
+        change('icon1');
+        body.classList.remove("light-mode");
+        body.classList.add("dark-mode");
+        element.classList.remove("light-modalName")
+        element.classList.add("dark-modalName");
+    } else {
+        change('icon1');
+        body.classList.remove("dark-mode");
+        body.classList.add("light-mode");
+        element.classList.remove("dark-modalName");
+        element.classList.add("light-modalName");
+    }
+}
